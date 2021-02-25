54211e310a2c63c163c780d49257f43bdb965743
CeruleanW <yyang.techie@gmail.com>
2021-02-24 20:46:26 -0500

    Add form submission

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 // include a contact form and your social media channels.
-import React from "react";
+import React from 'react';
 import {
   Typography,
   Paper,
@@ -8,31 +8,32 @@ import {
   Grid,
   TextField,
   useMediaQuery,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import { makeStyles } from "@material-ui/core/styles";
-import SocialMedia from "./SocialMedia";
-import { useTheme } from "@material-ui/styles";
-import { motion } from "framer-motion";
+import { makeStyles } from '@material-ui/core/styles';
+import SocialMedia from './SocialMedia';
+import { useTheme } from '@material-ui/styles';
+import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles((theme) => {
   const py = theme.spacing(10);
   return {
     textArea: {
       // maxWidth: "90vw",
-      width: "100%",
+      width: '100%',
       fontSize: theme.spacing(2.1),
     },
     submit: {
-      borderRadius: "0",
+      borderRadius: '0',
       marginTop: theme.spacing(1),
     },
     root: {
       padding: `${py}px ${theme.spacing(8)}px`,
-      [theme.breakpoints.up("md")]: {
+      [theme.breakpoints.up('md')]: {
         padding: `${py}px ${theme.spacing(20)}px`,
       },
-      [theme.breakpoints.up("lg")]: {
+      [theme.breakpoints.up('lg')]: {
         padding: `${py}px ${theme.spacing(30)}px`,
       },
     },
@@ -40,15 +41,18 @@ const useStyles = makeStyles((theme) => {
 });
 
 const SimpleTextField = (props) => {
-  const { label } = props;
+  const { label, inputRef, name } = props;
+  // ref={register}
   return (
     <TextField
       label={label}
-      variant="filled"
-      margin="normal"
+      variant='filled'
+      margin='normal'
       required
       fullWidth
-      color={"secondary"}
+      color={'secondary'}
+      inputRef={inputRef}
+      name={name}
     />
   );
 };
@@ -56,16 +60,27 @@ const SimpleTextField = (props) => {
 export default function Contact(props) {
   const classes = useStyles();
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("xs"), {
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'), {
     defaultMatches: true,
   });
 
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    //submit data to MailerSend Email API
+    const requestOptions = createRequestOption(data);
+    await fetch('https://api.mailersend.com/v1/email', requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error:', error));
+  };
+
   return (
     <Box maxWidth={1280} className={classes.root}>
-      <Typography variant={"h2"} component={"p"}>
+      <Typography variant={'h2'} component={'p'}>
         Get in touch?
       </Typography>
-      <Typography variant={"h2"} component={"p"}>
+      <Typography variant={'h2'} component={'p'}>
         Drop me a line!
       </Typography>
       <Box mt={1}>
@@ -76,39 +91,40 @@ export default function Contact(props) {
       </Box>
       <Box maxWidth={850} mt={4}>
         <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-          <form action="#">
-            <Grid container justify="center">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container justify='center'>
               <Grid container item spacing={isMobile ? 0 : 4}>
                 <Grid item xs={12} sm={6}>
-                  <SimpleTextField label="Name" />
+                  <SimpleTextField label='Name' inputRef={register} name={"name"} />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                  <SimpleTextField label="Email" />
+                  <SimpleTextField label='Email' inputRef={register} name={"email"} />
                 </Grid>
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  label={"Message"}
-                  variant="filled"
-                  margin="normal"
+                  label={'Message'}
+                  name={'message'}
+                  variant='filled'
+                  margin='normal'
                   required
                   fullWidth
-                  color={"secondary"}
+                  color={'secondary'}
                   multiline={true}
                   rows={5}
+                  inputRef={register}
                 />
               </Grid>
             </Grid>
             <Button
               className={classes.submit}
-              variant="contained"
-              color="primary"
-              size="large"
-              endIcon={""}
-              href={""}
-              target="_blank"
-              rel="noopener"
-              disabled
+              variant='contained'
+              color='primary'
+              size='large'
+              endIcon={''}
+              target='_blank'
+              rel='noopener'
+              type='submit'
             >
               Submit
             </Button>
@@ -119,9 +135,39 @@ export default function Contact(props) {
         {/* <Typography>
           Email: yyang.techie#gmail.com {"(Please replace # with @)"}
         </Typography> */}
-        <SocialMedia text={""} />
+        <SocialMedia text={''} />
       </Box>
     </Box>
   );
 }
 
+function createRequestOption(data) {
+  let myHeaders = new Headers();
+  myHeaders.append(
+    'Authorization',
+    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjI1YjlhZDNiZGViYWY0YzBkY2IyMmFjNzIwZWI4N2EzNDI5MDMzMWJhMGM2MTViMjdmOGM2ZWNhOWNhYTI3NDg4M2U0YzI1YTg2YmY2MGQiLCJpYXQiOjE2MTQyMDYzMjksIm5iZiI6MTYxNDIwNjMyOSwiZXhwIjo0NzY5ODc5OTI5LCJzdWIiOiIyNjUwIiwic2NvcGVzIjpbImVtYWlsX2Z1bGwiXX0.OqE-lpZc8I0SrijQ4KNt0bc6dpO_aepd4dEM9x805BvRvZGrKcj2cNpJVL5Vn-qGffIuhnQCihjOHa1fsuG80INWZi2OYXMZNfCBG4ffu4CJge_97zpXQiN4DOp_fHP-8GuP4-Gn_h8cJr8SDfM8IPY0X6dTI2u9UH_QqzIMRYsJ1Q1hqY1ETzUvVNb9JBYP6WO3FfMGtTBS4eRhATYYeKBYrXWACR6IjaqojphuKnTzyn0t6ObHq1fwDLf4nQh22ENnsxgA-Jr9xe3DAR4_BKc4BYwYobTB2htskLOvFDkDsK6KSnPtLvL2GutlYVGVt2A0KCEx3sUSy0It1CvXsK481zxat51k_U0IP4AlOvoarMI_qfWT1HfLEH-wGz01lvbRaDAA93t-eu-ebtETQP1Kk4933M6p6Azj1SImjszh8xxW2mhQD1QC6yoVFCrfd1I576MriHsneJIcRKVTyiW42J8M6SrDGdbalWezDVj17_VYMzPAqk9NNrA-VmwYFBumypHluY7NSGjBP-69aVltrwCMv8K1jLnnmoNcbECuEuneai1KyvndgTFetT5prQSoJ15ftFkR0dNJBSMbcWGvqfuwPCLiNNIpLKO-7_j-yvf-6KTQpdYwMA8TyDgKkwyIVPs0VBps8VbH9AYvnWwqP9h0mqR75RlW9GTD5j4'
+  );
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append(
+    'Cookie',
+    '__cfduid=d18414ef81bb46bb8b038f62e585ee9d81614206647'
+  );
+
+  const message = `This is a message from ${data.name}: \n ${data.message} \n Email: ${data.email}`;
+  const raw = JSON.stringify({
+    from: { email: 'hello@yiyangdev.me', name: 'MailerSend' },
+    to: [{ email: 'yyang.techie@gmail.com', name: 'Yi Yang' }],
+    subject: `Contact from portfolio | by ${data.name}`,
+    text: message,
+    html:
+      `<b>${message}</b>`,
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+  return requestOptions;
+}
