38fb779a47e9272add79fdbeb41eaaed732a4738
CeruleanW <yyang.techie@gmail.com>
2021-02-26 14:48:58 -0500

    Contact form submission will send email notification

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 // include a contact form and your social media channels.
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Typography,
   Paper,
@@ -8,6 +8,8 @@ import {
   Grid,
   TextField,
   useMediaQuery,
+  Modal,
+  CircularProgress
 } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -37,6 +39,12 @@ const useStyles = makeStyles((theme) => {
         padding: `${py}px ${theme.spacing(30)}px`,
       },
     },
+    spinner: {
+      position: 'absolute',
+      top: `50%`,
+      left: `50%`,
+      border: 'none'
+    }
   };
 });
 
@@ -57,22 +65,68 @@ const SimpleTextField = (props) => {
   );
 };
 
+function createRequestOption(data) {
+  const myHeaders = createHeader();
+
+  // const message = `This is a message from ${data.name}: \n ${data.message} \n Email: ${data.email}`;
+  const raw = JSON.stringify({
+    ...data,
+    // from: { email: 'hello@yiyangdev.me', name: 'MailerSend' },
+    // to: [{ email: 'yyang.techie@gmail.com', name: 'Yi Yang' }],
+    // subject: `Contact from portfolio | by ${data.name}`,
+    // text: message,
+    // html: `<b>${message}</b>`,
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+    // mode: 'no-cors',
+  };
+  return requestOptions;
+}
+
+function createHeader() {
+  let myHeaders = {};
+  Object.assign(myHeaders, {
+    Authorization:
+      'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjI1YjlhZDNiZGViYWY0YzBkY2IyMmFjNzIwZWI4N2EzNDI5MDMzMWJhMGM2MTViMjdmOGM2ZWNhOWNhYTI3NDg4M2U0YzI1YTg2YmY2MGQiLCJpYXQiOjE2MTQyMDYzMjksIm5iZiI6MTYxNDIwNjMyOSwiZXhwIjo0NzY5ODc5OTI5LCJzdWIiOiIyNjUwIiwic2NvcGVzIjpbImVtYWlsX2Z1bGwiXX0.OqE-lpZc8I0SrijQ4KNt0bc6dpO_aepd4dEM9x805BvRvZGrKcj2cNpJVL5Vn-qGffIuhnQCihjOHa1fsuG80INWZi2OYXMZNfCBG4ffu4CJge_97zpXQiN4DOp_fHP-8GuP4-Gn_h8cJr8SDfM8IPY0X6dTI2u9UH_QqzIMRYsJ1Q1hqY1ETzUvVNb9JBYP6WO3FfMGtTBS4eRhATYYeKBYrXWACR6IjaqojphuKnTzyn0t6ObHq1fwDLf4nQh22ENnsxgA-Jr9xe3DAR4_BKc4BYwYobTB2htskLOvFDkDsK6KSnPtLvL2GutlYVGVt2A0KCEx3sUSy0It1CvXsK481zxat51k_U0IP4AlOvoarMI_qfWT1HfLEH-wGz01lvbRaDAA93t-eu-ebtETQP1Kk4933M6p6Azj1SImjszh8xxW2mhQD1QC6yoVFCrfd1I576MriHsneJIcRKVTyiW42J8M6SrDGdbalWezDVj17_VYMzPAqk9NNrA-VmwYFBumypHluY7NSGjBP-69aVltrwCMv8K1jLnnmoNcbECuEuneai1KyvndgTFetT5prQSoJ15ftFkR0dNJBSMbcWGvqfuwPCLiNNIpLKO-7_j-yvf-6KTQpdYwMA8TyDgKkwyIVPs0VBps8VbH9AYvnWwqP9h0mqR75RlW9GTD5j4',
+  });
+  Object.assign(myHeaders, { 'Content-Type': 'application/json' });
+  // Object.assign(myHeaders, {'Access-Control-Allow-Origin': 'https://yiyangdev.me'});
+  Object.assign(myHeaders, { 'Access-Control-Allow-Origin': '*' });
+  // myHeaders.append(
+  //   'Cookie',
+  //   '__cfduid=d18414ef81bb46bb8b038f62e585ee9d81614206647'
+  // );
+  return myHeaders;
+}
+
 export default function Contact(props) {
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'), {
     defaultMatches: true,
   });
+  const [isReady, setIsReady] = useState(true);
 
   const { register, handleSubmit, watch, errors } = useForm();
   const onSubmit = async (data) => {
-    console.log(data);
-    //submit data to MailerSend Email API
+    setIsReady(false);
+    //submit data to API for notification
     const requestOptions = createRequestOption(data);
     console.log(requestOptions);
-    await fetch('https://api.mailersend.com/v1/email', requestOptions)
+    await fetch(
+      'https://email-service-cerulean.herokuapp.com/portfolio',
+      requestOptions
+    )
       .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((result) => {
+        alert('Success!');
+        setIsReady(true);
+      })
       .catch((error) => console.log('error:', error));
   };
 
@@ -134,6 +188,7 @@ export default function Contact(props) {
               target='_blank'
               rel='noopener'
               type='submit'
+              onClick={handleSubmit(onSubmit)}
             >
               Submit
             </Button>
@@ -146,39 +201,14 @@ export default function Contact(props) {
         </Typography> */}
         <SocialMedia text={''} />
       </Box>
+      <Modal
+        open={!isReady}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+        disableEnforceFocus
+      >
+        <CircularProgress className={classes.spinner}/>
+      </Modal>
     </Box>
   );
-}
-
-function createRequestOption(data) {
-  let myHeaders = {};
-  Object.assign(myHeaders, {
-    Authorization:
-      'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjI1YjlhZDNiZGViYWY0YzBkY2IyMmFjNzIwZWI4N2EzNDI5MDMzMWJhMGM2MTViMjdmOGM2ZWNhOWNhYTI3NDg4M2U0YzI1YTg2YmY2MGQiLCJpYXQiOjE2MTQyMDYzMjksIm5iZiI6MTYxNDIwNjMyOSwiZXhwIjo0NzY5ODc5OTI5LCJzdWIiOiIyNjUwIiwic2NvcGVzIjpbImVtYWlsX2Z1bGwiXX0.OqE-lpZc8I0SrijQ4KNt0bc6dpO_aepd4dEM9x805BvRvZGrKcj2cNpJVL5Vn-qGffIuhnQCihjOHa1fsuG80INWZi2OYXMZNfCBG4ffu4CJge_97zpXQiN4DOp_fHP-8GuP4-Gn_h8cJr8SDfM8IPY0X6dTI2u9UH_QqzIMRYsJ1Q1hqY1ETzUvVNb9JBYP6WO3FfMGtTBS4eRhATYYeKBYrXWACR6IjaqojphuKnTzyn0t6ObHq1fwDLf4nQh22ENnsxgA-Jr9xe3DAR4_BKc4BYwYobTB2htskLOvFDkDsK6KSnPtLvL2GutlYVGVt2A0KCEx3sUSy0It1CvXsK481zxat51k_U0IP4AlOvoarMI_qfWT1HfLEH-wGz01lvbRaDAA93t-eu-ebtETQP1Kk4933M6p6Azj1SImjszh8xxW2mhQD1QC6yoVFCrfd1I576MriHsneJIcRKVTyiW42J8M6SrDGdbalWezDVj17_VYMzPAqk9NNrA-VmwYFBumypHluY7NSGjBP-69aVltrwCMv8K1jLnnmoNcbECuEuneai1KyvndgTFetT5prQSoJ15ftFkR0dNJBSMbcWGvqfuwPCLiNNIpLKO-7_j-yvf-6KTQpdYwMA8TyDgKkwyIVPs0VBps8VbH9AYvnWwqP9h0mqR75RlW9GTD5j4',
-  });
-  Object.assign(myHeaders, {'Content-Type': 'application/json'});
-  // Object.assign(myHeaders, {'Access-Control-Allow-Origin': 'https://yiyangdev.me'});
-  // Object.assign(myHeaders, {'Access-Control-Allow-Origin': '*'});
-  // myHeaders.append(
-  //   'Cookie',
-  //   '__cfduid=d18414ef81bb46bb8b038f62e585ee9d81614206647'
-  // );
-
-  const message = `This is a message from ${data.name}: \n ${data.message} \n Email: ${data.email}`;
-  const raw = JSON.stringify({
-    from: { email: 'hello@yiyangdev.me', name: 'MailerSend' },
-    to: [{ email: 'yyang.techie@gmail.com', name: 'Yi Yang' }],
-    subject: `Contact from portfolio | by ${data.name}`,
-    text: message,
-    html: `<b>${message}</b>`,
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow',
-    // mode: 'no-cors',
-  };
-  return requestOptions;
 }
